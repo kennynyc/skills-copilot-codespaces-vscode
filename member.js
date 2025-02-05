6ceca636480f9f88b08f0f89036113ce91fe7494
kennynyc <kenny.wang@outlook.com>
2025-02-05 15:24:49 +0000

    2nd commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+  const skills = {
+    html: "advanced",
+    css: "advanced",
+    javascript: "advanced",
+    react: "intermediate",
+    node: "intermediate",
+    express: "intermediate",
+    mongo: "intermediate",
+    git: "intermediate",
+    github: "intermediate",
+    linux: "intermediate",
+    python: "beginner",
+    c: "beginner",
+    cplusplus: "beginner",
+    java: "beginner",
+    sql: "beginner",
+  };
+  return skills;
+}
